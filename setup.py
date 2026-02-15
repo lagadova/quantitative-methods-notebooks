55764d592f7d7020eabcf7cb9244d18e1d35c04c
Ладелаида Лагадова <ladelaida.lagadova@gmail.com>
2026-02-16 00:21:06 +0100

    QR code

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import qrcode
 import yaml
 
 def create_qr_code(url: str):
-   qr = qrcode.QRCode(
+    qr = qrcode.QRCode(
        version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
        border=4,
-   )
+    )
 
     qr.add_data(url)
     qr.make()
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color="purple", back_color="white")
 
     return img
 
