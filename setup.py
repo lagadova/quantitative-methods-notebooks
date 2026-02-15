da1f3c2774312c51bab8a57785c9ff2af2a63223
Ладелаида Лагадова <ladelaida.lagadova@gmail.com>
2026-02-15 18:14:54 +0100

    change qr code try 1

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
 import qrcode
-import yaml
+import os
+
+target_url = "https://lagadova.github.io/quantitative-methods-notebooks/"
+
+output_path = "figures/qr_code.png"
+
+qr = qrcode.QRCode(box_size=10, border=4)
+qr.add_data(target_url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save(output_path)
+
+#import qrcode
+#import yaml
+
+#def create_qr_code(url: str):
+   # qr = qrcode.QRCode(
+   #     version=1,
+   #     error_correction=qrcode.constants.ERROR_CORRECT_L,
+   #     box_size=10,
+    #    border=4,
+   # )
+
+   # qr.add_data(url)
+    #qr.make()
+    #img = qr.make_image(fill_color="black", back_color="white")
+
+    #return img
 
 
-def create_qr_code(url: str):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-
-    qr.add_data(url)
-    qr.make()
-    img = qr.make_image(fill_color="black", back_color="white")
-
-    return img
-
-
-with open('_variables.yml', 'r') as file:
-    cs = yaml.load(file, Loader=yaml.FullLoader)
-    qr_img = create_qr_code(cs['url'])
-    qr_img.save('figures/qr_code.png')
+#with open('_variables.yml', 'r') as file:
+ #   cs = yaml.load(file, Loader=yaml.FullLoader)
+  #  qr_img = create_qr_code(cs['url'])
+   # qr_img.save('figures/qr_code.png')
+##
